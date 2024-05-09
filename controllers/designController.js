5a70d7963eeca2c7cf9040f7ef7371859a9400cd
Karan <karanworks16@gmail.com>
2024-05-09 16:47:58 +0530

    delte ivr design api

--- controllers/designController.js
+++ controllers/designController.js
@@ -38,27 +38,34 @@ class DesignController {
             },
           });
 
-          // filter parent designs
+          async function fetchItemsRecursively(designId) {
+            const items = await prisma.ivrDesign.findMany({
+              where: {
+                parentId: designId,
+              },
+            });
+
+            // Recursively fetch items for each item
+            const nestedItems = await Promise.all(
+              items.map(async (item) => {
+                item.items = await fetchItemsRecursively(item.id);
+                return item;
+              })
+            );
+
+            return nestedItems;
+          }
+
           const parentDesigns = loggedInUser.designs.filter((design) => {
             return !design.parentId;
           });
 
           const parentDesignWithItems = await Promise.all(
             parentDesigns.map(async (design) => {
-              const items = await prisma.ivrDesign.findMany({
-                where: {
-                  parentId: design.id,
-                },
-              });
-
-              return {
-                ...design,
-                items,
-              };
+              design.items = await fetchItemsRecursively(design.id);
+              return design;
             })
           );
-
-          console.log("parent with items ->", parentDesignWithItems);
 
           const { password, ...adminDataWithoutPassword } = loggedInUser;
 
@@ -82,11 +89,10 @@ class DesignController {
 
   async createDesignPost(req, res) {
     try {
-      const { audioText, audioFile, ivrCampaignId, key, parentId } = req.body;
+      const { audioText, audioFile, ivrCampaignId, key, parentId, number } =
+        req.body;
 
       const token = await getToken(req, res);
-
-      console.log("audio text here ->", audioText);
 
       // admin that is creating the user
       const adminUser = await prisma.user.findFirst({
@@ -95,25 +101,179 @@ class DesignController {
         },
       });
 
-      if (adminUser) {
-        const newIvrDesign = await prisma.ivrDesign.create({
-          data: {
-            audioText,
-            ivrCampaignId,
-            key,
-            createdBy: adminUser.id,
-            parentId: parentId ? parentId : null,
+      if (parentId) {
+        const parent = await prisma.ivrDesign.findFirst({
+          where: {
+            id: parentId,
           },
         });
 
-        console.log("newly created ivr design ->", newIvrDesign);
+        const childs = await prisma.ivrDesign.findMany({
+          where: {
+            parentId: parent.id,
+          },
+        });
 
-        response.success(res, "new ivr design created!", newIvrDesign);
+        const parentItemsKeys = childs?.map((child) => {
+          return child.key;
+        });
+
+        const itemBindedWithSameKey = parentItemsKeys.includes(key);
+        console.log("CHECKING BINDING ->", key);
+        if (itemBindedWithSameKey) {
+          console.log("YES IT INCLUDES");
+          response.error(res, "Item already exists on same key");
+          return;
+        }
+      }
+
+      if (adminUser) {
+        if (number) {
+          const parent =
+            parentId &&
+            (await prisma.ivrDesign.findFirst({
+              where: {
+                parentId: parentId,
+                isNumber: false,
+              },
+            }));
+          if (parent) {
+            response.error(res, "Keys bindings already exists!");
+            return;
+          }
+
+          const newIvrDesign = await prisma.ivrDesign.create({
+            data: {
+              ivrCampaignId,
+              createdBy: adminUser.id,
+              parentId: parentId ? parentId : null,
+              number,
+              isNumber: true,
+            },
+          });
+
+          response.success(res, "new ivr design created!", newIvrDesign);
+        } else {
+          const parent =
+            parentId &&
+            (await prisma.ivrDesign.findFirst({
+              where: {
+                parentId: parentId,
+                isNumber: true,
+              },
+            }));
+
+          if (parent) {
+            response.error(res, "Number binding already exists!");
+            return;
+          }
+
+          const newIvrDesign = await prisma.ivrDesign.create({
+            data: {
+              audioText,
+              ivrCampaignId,
+              key,
+              createdBy: adminUser.id,
+              parentId: parentId ? parentId : null,
+              isNumber: false,
+            },
+          });
+
+          response.success(res, "new ivr design created!", newIvrDesign);
+        }
       } else {
         response.error(res, "User doesn't exist!");
       }
     } catch (error) {
       console.log("error while creating ivr design ->", error);
+    }
+  }
+
+  // async designRemoveDelete(req, res) {
+  //   try {
+  //     const { designId } = req.params;
+
+  //     console.log("IVR DESIGN API DELETE API CALLED");
+  //     // finding campaign from campaignId
+  //     const designFound = await prisma.ivrDesign.findFirst({
+  //       where: {
+  //         id: parseInt(designId),
+  //       },
+  //     });
+
+  //     if (designFound) {
+  //       if (!designFound.parentId) {
+  //         const childs = await prisma.ivrDesign.findMany({
+  //           where: {
+  //             parentId: designFound.id,
+  //           },
+  //         });
+  //       }
+
+  //       response.success(res, "Design deleted successfully");
+  //     } else {
+  //       response.error(res, "Design does not exist!");
+  //     }
+  //   } catch (error) {
+  //     console.log("error while deleting campaign ", error);
+  //   }
+  // }
+
+  async designRemoveDelete(req, res) {
+    try {
+      const { designId } = req.params;
+
+      console.log("DESIGN ID ->", designId);
+
+      // finding campaign from campaignId
+      const designFound = await prisma.ivrDesign.findFirst({
+        where: {
+          id: parseInt(designId),
+        },
+      });
+
+      if (designFound) {
+        // Initialize an array to store all child records
+        let allChilds = [];
+
+        // Fetch all child records recursively
+        async function getAllChilds(parentId) {
+          const childs = await prisma.ivrDesign.findMany({
+            where: {
+              parentId: parentId,
+            },
+          });
+
+          for (const child of childs) {
+            allChilds.push(child);
+            await getAllChilds(child.id);
+          }
+        }
+
+        await getAllChilds(designFound.id);
+
+        const idOfChilds = allChilds.map((child) => child.id);
+
+        const deletedChilds = await prisma.ivrDesign.deleteMany({
+          where: {
+            id: {
+              in: idOfChilds,
+            },
+          },
+        });
+
+        const deletedParent = await prisma.ivrDesign.delete({
+          where: {
+            id: designFound.id,
+          },
+        });
+
+        response.success(res, "Design deleted successfully", { deletedParent });
+      } else {
+        response.error(res, "Design does not exist!");
+      }
+    } catch (error) {
+      console.log("error while deleting ivr design ", error);
     }
   }
 }
--- routes/design.js
+++ routes/design.js
@@ -4,5 +4,9 @@ const DesignController = require("../controllers/designController");
 
 DesignRouter.get("/ivr-design", DesignController.getDesign);
 DesignRouter.post("/ivr-design/create", DesignController.createDesignPost);
+DesignRouter.delete(
+  "/ivr-design/:designId/delete",
+  DesignController.designRemoveDelete
+);
 
 module.exports = DesignRouter;
